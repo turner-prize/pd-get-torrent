a23bfd23c662c7a7565066340ecc6b913de0e633
turner-prize <hiphoperation@hotmail.com>
2018-07-21 10:53:23 +0100

    Update script.py
    more old changes!

--- script.py
+++ script.py
@@ -1,29 +1,46 @@
-#!/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 22:03:20 2018
 
-import os
+@author: Turner_prize
+"""
+
 import re
-import json
-import requests
+import os
+from json import dumps
 import datetime
 import time
 from bs4 import BeautifulSoup
-#from collections import Counter
+import requests
+
+# Replace hostname, transmission-username and transmission-password
+
+TVDB = "empty .txt file"
+MyDir = "final folder"
 
 
-RegDate = re.compile(r'\d{1,2}_\d{1,2}_\d{4}')
-showList = []
-ShowNames = []
-data = {}
-    
+def AmendList(ShowList):
+    f = open(TVDB, 'r')
+    ShowDatabase = f.read().splitlines()
+    for Show in ShowList:
+        if not Show in ShowDatabase:
+            ShowDatabase.append(Show)
+    with open(TVDB, 'w') as outfile:
+        for item in ShowDatabase:
+            outfile.write("%s\n" % item)
+    return ShowDatabase
+
 def CreateShowList(MyPath):
 #checks folder for TV Shows, adds them to list to compare later.
+    ShowList= []
+    ShowNames = []
     findit = re.compile(r'.S\d{1,2}E\d{1,2}')
     sYear = re.compile(r'\d{4}\s')
     for dirname, dirnames, filenames in os.walk(MyPath,topdown=False):
         for files in filenames:
             foundit = re.search(findit,files.upper())
             if foundit:
-                rubbish =  foundit.group()
+                rubbish = foundit.group()
                 x = files.upper().find(rubbish) #x is an integer
                 MyShow =  files[:x].title() + rubbish
                 MyShow = MyShow.replace("."," ")
@@ -31,59 +48,38 @@ def CreateShowList(MyPath):
                 if foundYear:
                     MyShow = re.sub(r'\d{4}\s',"",MyShow)
                 if not files[:x].title() in ShowNames:
-                    ShowNames.append(MyShow.upper())
-    AmendList(ShowNames)
-    
-def AmendList(gotShows):
-    with open("C:\Users\Turner_prize\Desktop\TVDB.txt", 'r') as infile:
-        global data
-        data = json.load(infile)
-    countr = len(data)
-    for Shows in gotShows:
-        if not Shows in data.values():
-            countr += 1
-            data[countr] = Shows
-    with open(r'C:\Users\Turner_prize\Desktop\TVDB.txt', 'w') as outfile:
-        json.dump(data, outfile)
+                    ShowList.append(MyShow.upper())
+    ShowList = sorted(ShowList)
+    ShowList = AmendList(ShowList)
+    return ShowList
 
 
-def GetThisMonth():
-    today = datetime.date.today()
-    NewDate= today.strftime("%m-%Y")
-    NewDate = NewDate.lstrip('0')  
+def GetMyDates(x):
+    NewDate = datetime.date.today()
+    if x > 0:
+        dt = datetime.timedelta(days=1)
+        NewDate = datetime.date(NewDate.year, NewDate.month, 1) - dt
+    if x > 1:
+        NewDate = datetime.date(NewDate.year, NewDate.month, 1) - dt
+    NewDate = NewDate.strftime("%m-%Y")
+    NewDate = NewDate.lstrip('0')
     return NewDate
-
-
-def GetLastMonth():
-    today = datetime.date.today()
-    dt = datetime.timedelta(days=1)
-    NewDate = datetime.date(today.year, today.month, 1) - dt
-    NewDate= NewDate.strftime("%m-%Y")
-    NewDate = NewDate.lstrip('0')  
-    return NewDate
-
-def Get2Months():
-
-    today = datetime.date.today()
-    dt = datetime.timedelta(days=1)
-    NewDate = datetime.date(today.year, today.month, 1) - dt
-    NewDate = datetime.date(NewDate.year, NewDate.month, 1) - dt
-    NewDate= NewDate.strftime("%m-%Y")
-    NewDate = NewDate.lstrip('0')  
-    return NewDate
-
-
 
 def GetPogDesignURL():
-        MyDate = GetThisMonth()
+    for i in range(3):
+        MyDate = GetMyDates(i)
+        print MyDate
         myURL = "http://www.pogdesign.co.uk/cat/" + MyDate
-        ShowRequest(myURL)
-
+        MyList = ShowRequest(myURL)
+        yield MyList
+        
 def ShowRequest(website):
+    NewShows = []
+    RegDate = re.compile(r'\d{1,2}_\d{1,2}_\d{4}')
     with requests.Session() as c:
         url = "https://www.pogdesign.co.uk/cat/login"
-        uName = "enter username here"
-        pWord = "enter password here"
+        uName = "username"
+        pWord = "password"
         c.get(url)
         login_data = dict(username=uName, password=pWord, sub_login="")
         c.post(url, data=login_data, headers={"Referer":"https://www.pogdesign.co.uk/cat/login"})
@@ -97,41 +93,42 @@ def ShowRequest(website):
             d = datetime.datetime.strptime(foundit.group(), '%d_%m_%Y').date()
         if d > datetime.date.today():
             break
-        
-        GetShowString("ep info",j)
-        GetShowString("ep info fep",j)
-        GetShowString("ep info pep",j)
-        GetShowString("ep info lep",j)
+
+        for i in GetShowString("ep info",j):
+            NewShows.append(i)
+        for i in GetShowString("ep info fep",j):
+            NewShows.append(i)
+        for i in GetShowString("ep info pep",j):
+            NewShows.append(i)
+        for i in GetShowString("ep info lep",j):
+            NewShows.append(i)
 
     for j in soup.find_all("div",class_="today"):
-        GetShowString("ep",j)
-    
-    global showList
-    showList = [w.strip('\n') for w in showList]
-    showList = [w.strip() for w in showList]
-    showList = [w.replace('\n'," ") for w in showList]
+        for i in GetShowString("ep",j):
+            NewShows.append(i)
 
-    print showList
-        
+    NewShows = filter(None, NewShows)
+    NewShows = [w.strip('\n') for w in NewShows]
+    NewShows = [w.strip() for w in NewShows]
+    NewShows = [w.replace('\n'," ") for w in NewShows]
+    return NewShows
+
+
 def GetShowString(iClass,showz):
     for i in showz.find_all("div",class_=iClass):
         fix = i.get_text()
-        print fix
-        showList.append(fix)
-        
+        yield fix
+
 def AddToList(NewShow):
     #this is similar to AmendList, but is specifically for when a torrent file is found.
-    with open("C:\Users\Turner_prize\Desktop\TVDB.txt", 'r') as infile:
-        global data
-        data = json.load(infile)
-    countr = len(data)
-    if not NewShow in data.values():
-        countr += 1
-        NewShow = NewShow.upper()
-        data[countr] = NewShow
-    with open(r'C:\Users\Turner_prize\Desktop\TVDB.txt', 'w') as outfile:
-        json.dump(data, outfile)
-    
+    f = open(TVDB, 'r')
+    ShowDatabase = f.read().splitlines()
+    ShowDatabase.append(NewShow.upper())
+    with open(TVDB, 'w') as outfile:
+        for item in ShowDatabase:
+            outfile.write("%s\n" % item)
+
+
 def GetTorrent(MyShow):
     r = requests.get("https://thepiratebay.org/search/" + MyShow +"/0/99/208")
     soop = BeautifulSoup(r.content, "lxml")
@@ -140,28 +137,22 @@ def GetTorrent(MyShow):
     else:
         for a in soop.find_all('a', href=True, title='Download this torrent using magnet'):
             if a:
-                print a['href']
                 os.startfile(a['href'])
                 AddToList(MyShow)
                 break
 
-def GetTorrentCount():
-    r = requests.get("http://localhost:6886/index.tmpl")
-    soup = BeautifulSoup(r.text,"lxml")
-    Torrents = soup.find(id="tab_selected")
-    Torrents = Torrents.text
-    Torrents = Torrents.replace("Downloads (","") 
-    Torrents = Torrents.replace(")","") 
-    return int(Torrents)
+ShowList = CreateShowList(MyDir)
 
-MyDir = (r'vuze download path here')
-CreateShowList(MyDir)
-GetPogDesignURL()
+ShowDatabase = sorted(ShowList)
 
-print showList
+with open(TVDB, 'w') as outfile:
+    for item in ShowDatabase:
+        outfile.write("%s\n" % item)
 
-for shows in showList:
-    if not shows.upper() in data.values():
-        print shows
-        GetTorrent(shows)
-        time.sleep(7)
+for Shows in GetPogDesignURL():
+    for Show in Shows:
+       if not Show.upper() in ShowList:
+               print Show.upper() 
+               GetTorrent(Show)
+               time.sleep(5)
+
